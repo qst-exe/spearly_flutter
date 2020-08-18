17ffe64f7fa2aa99cb11968a112a3bd53a0a96a1
Task Nagashige <task.19.xx@gmail.com>
2020-08-18 11:01:14 +0900

    Spearlyプラグインの作成

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:spearly_flutter/spearly_flutter.dart';
 
 void main() {
@@ -14,32 +11,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await SpearlyFlutter.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -47,10 +21,12 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Plugin Spearly Flutter'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: SpearlyFlutter(
+          apiKey: "Bearer xxxxx",
+          contentId: "c-yyyyy",
+          disableImage: true,
         ),
       ),
     );
--- lib/spearly_flutter.dart
+++ lib/spearly_flutter.dart
@@ -1,13 +1,152 @@
-import 'dart:async';
+library spearly_flutter;
 
-import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class SpearlyFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('spearly_flutter');
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'package:webview_flutter/webview_flutter.dart';
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+const String _html = '''
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Title</title>
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+</head>
+<body>
+<div data-role="page" data-title="Sample1">
+  <div data-role="header">
+  </div>
+  <div data-role="content">
+    <spearly-content>
+      <h1>{%title%}</h1>
+      <img src="{%image%}" />
+      <p>{%description%}</p>
+      {%body%}
+    </spearly-content>
+  </div>
+  <div data-role="footer">
+  </div>
+</div>
+</body>
+</html>
+''';
+
+class SpearlyFlutter extends StatefulWidget {
+  final String apiKey;
+  final String contentId;
+  final double width;
+  final double height;
+  final bool disableTitle;
+  final bool disableImage;
+  final bool disableDescription;
+  final bool disableBody;
+
+  SpearlyFlutter({
+    @required this.apiKey,
+    @required this.contentId,
+    this.width,
+    this.height,
+    this.disableTitle = false,
+    this.disableImage = false,
+    this.disableDescription = false,
+    this.disableBody = false,
+  });
+
+  @override
+  _SpearlyFlutterState createState() => _SpearlyFlutterState();
+}
+
+class _SpearlyFlutterState extends State<SpearlyFlutter> {
+  WebViewController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+    double _screenHeight = MediaQuery.of(context).size.height;
+
+    double _contentWidth = (widget.width != null) ? widget.width : _screenWidth;
+    double _contentHeight =
+        (widget.height != null) ? widget.height : _screenHeight;
+
+    return FutureBuilder(
+      future: _getContent(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snap) {
+        if (snap.hasData && !snap.hasError) {
+          final fields = snap.data["fields"];
+          final String _title =
+              (fields["title"] != null) ? fields["title"]["value"] : "";
+          final String _image =
+              (fields["image"] != null) ? fields["image"]["value"] : "";
+          final String _body =
+              (fields["body"] != null) ? fields["body"]["value"] : "";
+          final String _description = (fields["description"] != null)
+              ? fields["description"]["value"]
+              : "";
+          return Container(
+              width: _contentWidth,
+              height: _contentHeight,
+              child: WebView(
+                onWebViewCreated: (WebViewController webViewController) async {
+                  _controller = webViewController;
+                  await _loadHtmlFromAssets(
+                      title: _title,
+                      image: _image,
+                      description: _description,
+                      body: _body);
+                },
+                javascriptMode: JavascriptMode.unrestricted,
+              ));
+        } else {
+          return SizedBox.shrink();
+        }
+      },
+    );
+  }
+
+  Future _loadHtmlFromAssets(
+      {String title, String image, String description, String body}) async {
+    String _htmlText = _html;
+    // タイトルの置換
+    if (widget.disableTitle == false) {
+      _htmlText = _htmlText.replaceAll('{%title%}', title);
+    } else {
+      _htmlText = _htmlText.replaceAll('''<h1>{%title%}</h1>''', "");
+    }
+    // メイン画像の置換
+    if (widget.disableImage == false) {
+      _htmlText = _htmlText.replaceAll('{%image%}', image);
+    }
+    // 説明文の置換
+    if (widget.disableDescription == false) {
+      _htmlText = _htmlText.replaceAll('{%description%}', description);
+    } else {
+      _htmlText = _htmlText.replaceAll('''<p>{%description%}</p>''', "");
+    }
+    // bodyの置換
+    if (widget.disableBody == false) {
+      _htmlText = _htmlText.replaceAll('{%body%}', body);
+    } else {
+      _htmlText = _htmlText.replaceAll('{%body%}', "");
+    }
+    await _controller.loadUrl(Uri.dataFromString(_htmlText,
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString());
+  }
+
+  _getContent() async {
+    final http.Response response = await http.get(
+      "https://www.spearly.com/api/v1/contents/${widget.contentId}",
+      headers: {HttpHeaders.authorizationHeader: widget.apiKey},
+    );
+    if (response.statusCode != 200) {
+      return;
+    }
+
+    return json.decode(response.body);
   }
 }
